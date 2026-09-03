90bc3ef88d71e52100c4928a1c4419fbdbaeb04a
M Aun Rasheed <greataun786@gmail.com>
2026-09-03 14:56:42 +0500

    refactor: addded cart context and made responsive the grid layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,15 +2,28 @@
 
 import { ShoppingBag } from 'lucide-react';
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hamburger from './UI/Hamburger';
 import { navOptions } from '@/constants/NavbarConst';
 import Image from 'next/image';
+import { useCart } from '@/app/context/CartContext';
 
 const Navbar = () => {
 
+    const [isScrolled, setIsScrolled] = useState(false)
+    const { toggleCart } = useCart()
+
+    useEffect(() => {
+        function handleScroll() {
+            setIsScrolled(window.scrollY > 100)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+
     return (
-        <nav className='px-5 lg:px-10 py-5 w-full h-35 bg-secondary grid grid-cols-[55%_45%] items-center fixed top-0 z-100 border-b border-accent'>
+        <nav className={`px-5 lg:px-10 py-5 w-full h-35 grid max-sm:grid-cols-[30%_30%_40%] grid-cols-[40%_25%_35%] md:grid-cols-[45%_25%_30%] items-center fixed top-0 z-100 border-b border-accent transition-all ease-linear duration-500 ${isScrolled ? 'bg-secondary/30 backdrop-blur-xl' : 'bg-secondary'}`}>
             <div className='flex items-center justify-between'>
                 <ul className='flex items-center justify-between gap-10 max-lg:hidden'>
                     {navOptions.map(option => {
@@ -27,15 +40,15 @@ const Navbar = () => {
                 <span className='lg:hidden'>
                     <Hamburger />
                 </span>
-                <Link href={'/'} className='relative'>
-                    <div className='absolute left-1/2 -top-5 -translate-x-1/2 -translate-y-1/2 h-10 w-10'>
-                        <Image src={'/logo.webp'} alt='Logo' fill sizes='100px' className='w-full h-full'/>
-                    </div>
-                    <h1 className='font-brand font-bold text-foreground tracking-widest text-4xl leading-none'>LUMÉA</h1>
-                </Link>
             </div>
+            <Link href={'/'} className='relative'>
+                {/* <div className='absolute left-1/2 -top-5 -translate-x-1/2 -translate-y-1/2 h-10 w-10'>
+                    <Image src={'/logo.webp'} alt='Logo' fill sizes='100px' className='w-full h-full' />
+                </div> */}
+                <h1 className='font-brand font-bold text-foreground tracking-widest text-4xl leading-none'>LUMÉA</h1>
+            </Link>
             <div className='flex justify-end'>
-                <span className='cursor-pointer text-foreground transition-all ease-linear duration-300 hover:-translate-y-1'>
+                <span onClick={toggleCart} className='cursor-pointer text-foreground transition-all ease-linear duration-300 hover:-translate-y-1'>
                     <ShoppingBag strokeWidth={1} />
                 </span>
             </div>
