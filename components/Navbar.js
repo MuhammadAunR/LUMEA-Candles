80d5cf8e1d5d8a212342283964ffe6297532c31f
M Aun Rasheed <greataun786@gmail.com>
2026-09-04 19:37:28 +0500

    feat: added cart item count to navbar cart icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,7 +11,7 @@ import { useCart } from '@/app/context/CartContext';
 const Navbar = () => {
 
     const [isScrolled, setIsScrolled] = useState(false)
-    const { toggleCart } = useCart()
+    const { toggleCart, cartItemsInLS, handleTotalCost } = useCart()
 
     useEffect(() => {
         function handleScroll() {
@@ -41,15 +41,25 @@ const Navbar = () => {
                     <Hamburger />
                 </span>
             </div>
-            <Link href={'/'} className='relative'>
-                {/* <div className='absolute left-1/2 -top-5 -translate-x-1/2 -translate-y-1/2 h-10 w-10'>
-                    <Image src={'/logo.webp'} alt='Logo' fill sizes='100px' className='w-full h-full' />
-                </div> */}
+            <Link href={'/'} className='relative w-fit'>
                 <h1 className='font-brand font-bold text-foreground tracking-widest text-4xl leading-none'>LUMÉA</h1>
+                <div className='absolute left-1/2 -top-5 -translate-x-1/2 -translate-y-1/2 h-10 w-10'>
+                    <Image src={'/logo.webp'} alt='Logo' fill sizes='100px' className='w-full h-full' />
+                </div>
             </Link>
             <div className='flex justify-end'>
-                <span onClick={toggleCart} className='cursor-pointer text-foreground transition-all ease-linear duration-300 hover:-translate-y-1'>
+                <span
+                    onClick={toggleCart}
+                    className='cursor-pointer text-foreground transition-all ease-linear duration-300 hover:-translate-y-1 relative flex items-center gap-1'>
                     <ShoppingBag strokeWidth={1} />
+                    {cartItemsInLS.length > 0 &&
+                        <span className='absolute -top-2 -left-3 font-heading bg-primary border rounded-full w-5 h-5 text-xs flex items-center justify-center'>
+                            {cartItemsInLS.length > 9 ? '9+' : cartItemsInLS.length}
+                        </span>
+                    }
+                    <span className='text-sm'>
+                        {handleTotalCost.toLocaleString()}
+                    </span>
                 </span>
             </div>
         </nav>
